b9cd8ebb1e76e3770a6dc13c40b1de2ead37b4cd
Jacob Logsdon <jrlogsdon21@gmail.com>
2022-01-11 22:16:00 -0500

    lambdas and higher order functions

---
+++ sample/src/main/kotlin/HigherOrderFunctions.kt
@@ -0,0 +1,26 @@
+
+fun main() {
+    var dirtyLevel = 20
+    val waterFilter = { dirty : Int -> dirty / 2}
+    // lambda
+    println(waterFilter(dirtyLevel))
+
+    dirtyLevel = updateDirty(30, waterFilter)
+    println(dirtyLevel)
+
+    println("\n-----\npassing in normal function as an operator to a higher order function")
+    println(updateDirty(20, ::increaseDirty))
+
+    println("\n\nspecial syntax for last parameter as lambda")
+    println(updateDirty(20) { it + 23})
+
+}
+
+// higher order function (takes a function)
+fun updateDirty(dirty : Int, operation: (Int) -> Int): Int {
+
+    return operation(dirty)
+}
+
+// compact function
+fun increaseDirty( start : Int) = start + 1
