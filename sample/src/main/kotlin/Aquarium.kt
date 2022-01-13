6345ffd6d1bb73347a64d59417ace5981bef4f0b
Jacob Logsdon <jrlogsdon21@gmail.com>
2022-01-13 13:37:57 -0500

    adding constructors and property getter and setter

--- sample/src/main/kotlin/Aquarium.kt
+++ sample/src/main/kotlin/Aquarium.kt
@@ -1,12 +1,19 @@
-class Aquarium {
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
+class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40){
+    init {
+        println("aquarium initializing")
+    }
+
+    var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
 
     fun printSize() {
         println("Width: $width cm " +
         "Length: $length cm " +
         "Height: $height cm")
+        println("Volume $volume l")
     }
 
 }
--- sample/src/main/kotlin/Classes.kt
+++ sample/src/main/kotlin/Classes.kt
@@ -1,9 +1,21 @@
-
 fun buildAquarium() {
     val myAquarium = Aquarium()
     myAquarium.printSize()
     myAquarium.height = 60
     myAquarium.printSize()
+
+    val aquarium1 = Aquarium(width = 25)
+    aquarium1.printSize()
+
+    val aquarium2 = Aquarium(height = 35, length = 110)
+    aquarium2.printSize()
+
+    val aquarium3 = Aquarium(width = 25, height = 55, length = 100)
+    aquarium3.printSize()
+
+    aquarium3.volume = 40
+    aquarium3.printSize()
+
 }
 
 fun main() {
