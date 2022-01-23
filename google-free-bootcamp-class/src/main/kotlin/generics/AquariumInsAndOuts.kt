abf6524f0101308a1ead4c5fa4a856ee4a898db6
Jacob Logsdon <jrlogsdon21@gmail.com>
2022-01-23 16:38:39 -0500

    exploring reified/real types that are determined at runtime in kotlin

--- google-free-bootcamp-class/src/main/kotlin/generics/AquariumInsAndOuts.kt
+++ google-free-bootcamp-class/src/main/kotlin/generics/AquariumInsAndOuts.kt
@@ -1,13 +1,15 @@
 package generics
 // key word out - lets the compiler ensure nothing unsafe is happening with the type
-class AquariumOut<out T: WaterSupply>(val waterSupply: T) {
+class AquariumForGenerics<T: WaterSupply>(val waterSupply: T) {
     fun addWater(cleaner: Cleaner<T>) {
         if (waterSupply.needsProcessing) {
             cleaner.clean(waterSupply)
         }
         println("water added")
     }
+
 }
+inline fun <reified R: WaterSupply> AquariumForGenerics<*>.hasWaterSupplyOfType() = waterSupply is R
 
 interface Cleaner<in T: WaterSupply> {
     fun clean(waterSupply : T)
@@ -17,14 +19,41 @@ class TapWaterCleaner : Cleaner<TapWater> {
     override fun clean (waterSupply: TapWater) = waterSupply.addChemicalCleaners()
 }
 
-fun addItemTo(aquarium: AquariumOut<WaterSupply>) = println("item added")
+fun addItemTo(aquarium: AquariumForGenerics<WaterSupply>) = println("item added")
+
+fun <T: WaterSupply> isWaterClean(aquarium: AquariumForGenerics<T>) {
+    println("aquarium water is clean: ${!aquarium.waterSupply.needsProcessing}")
+}
+
+class OceanWater : WaterSupply(false) {}
 
 fun generic() {
     val cleaner = TapWaterCleaner()
-    val aquarium = AquariumOut(TapWater())
+    val aquarium = AquariumForGenerics(TapWater())
     aquarium.addWater(cleaner)
+}
+
+fun genericTwo() {
+    val aquarium = AquariumForGenerics(TapWater())
+    isWaterClean(aquarium)
+}
+
+fun genericUsingReifiedVar() {
+    val aquarium = AquariumForGenerics(TapWater())
+    println(aquarium.hasWaterSupplyOfType<TapWater>())
+    println(aquarium.hasWaterSupplyOfType<OceanWater>())
 }
 
 fun main() {
     generic()
+    println("----\npart2")
+    genericTwo()
+    println("----\npart3")
+    genericUsingReifiedVar()
+}
+
+class TapWaterOut : WaterSupply(true) {
+    fun addChemicalCleaners() {
+        needsProcessing = false
+    }
 }
