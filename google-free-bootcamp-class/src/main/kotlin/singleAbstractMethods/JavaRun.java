9a41995024f824c9de54149b4aa5b3a5ce984cef
Jacob Logsdon <jrlogsdon21@gmail.com>
2022-01-26 22:06:28 -0500

    Adding SAM example with Java Runnable

---
+++ google-free-bootcamp-class/src/main/kotlin/singleAbstractMethods/JavaRun.java
@@ -0,0 +1,7 @@
+package singleAbstractMethods;
+
+public class JavaRun {
+    public static void runNow(Runnable runnable) {
+        runnable.run();
+    }
+}
---
+++ google-free-bootcamp-class/src/main/kotlin/singleAbstractMethods/Runnable.kt
@@ -0,0 +1,12 @@
+package singleAbstractMethods
+
+fun runExample() {
+    JavaRun.runNow{
+        println("Passing a lambda (last parameter) as a runnable")
+    }
+
+}
+
+fun main() {
+    runExample()
+}
